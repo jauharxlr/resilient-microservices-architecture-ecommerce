843fc6c810287cf645afe3ce37f6c033194ebc93
jauharxlr <jauharxlr@gmail.com>
2024-11-30 11:21:15 +0400

    Added notification service Added payment service Configured root gradle

---
+++ notification-service/src/main/java/com/example/notificationservice/constant/MetricConstants.java
@@ -0,0 +1,15 @@
+package com.example.notificationservice.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum MetricConstants {
+
+    ORDER_REQUEST("order_request","Counter for tracking order requests", new String[]{"service", "order-service", "controller", "OrderController"}),
+    ORDER_REQUEST_RESPONSE_TIME("order_request_response_time","Guage for tracking order requests response time", new String[]{"service", "order-service", "controller", "OrderController"});
+    private final String metric;
+    private final String description;
+    private final String[] tags;
+}
